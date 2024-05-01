784da9c1d67494de586e98b74838b05b02ef6a4e
Chao Moua <chaomoua91@gmail.com>
2024-05-01 18:15:42 -0500

    adding javascript codes to create books

--- script.js
+++ script.js
@@ -1,11 +1,40 @@
 // console.log('hello world')
 
-const myLibrary = [];
+const books = document.querySelector('.books')
 
-function Book() {
-  // the constructor...
+const myLibrary = [
+    { 
+        title: "Book1",
+        author: "me",
+        pages: 500,
+        read: true,
+    },
+    { 
+        title: "Book2",
+        author: "you",
+        pages: 5000,
+        read: false,
+    },
+];
+
+function createBookElement(ele, content, className) {
+    const element = document.createElement(ele);
+    element.textContent = content;
+    element/setAttribute("class", className);
+    return element;
+
 }
 
-function addBookToLibrary() {
-  // do stuff here
+function createBookItem(book,index) {
+    const bookItem = document.createElement('div');
+    bookItem.setAttribute('id', index)
+    bookItem.setAttribute('key', index)
+    bookItem.setAttribute('class', 'card book')
+    bookItem.appendChild(createBookElement('h1', "Title" + ))
+}
+
+function renderBooks() {
+  myLibrary.map((book,index) => {
+    createBookItem(book,index)
+  })
 }
